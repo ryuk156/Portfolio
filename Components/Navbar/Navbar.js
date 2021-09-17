c8d1b144e087602a8e0e2302cd68b9cb4b28bbf8
ryuk156 <yp15601560@gmail.com>
2021-09-18 01:20:56 +0530

    add: animation to theme manager

--- Components/Navbar/Navbar.js
+++ Components/Navbar/Navbar.js
@@ -1,20 +1,18 @@
+/* eslint-disable @next/next/no-img-element */
 import ThemeManager from "../ThemeManager/ThemeManager";
-import Image from "next/image";
 
 const Navbar = () => {
   return (
     <div className="nav">
       <div className="nav-brand">
-        <Image
+        <img
           src="/images/me.png"
           alt="Picture of the author"
           className="nav-brand-profile"
-          width={48}
-          height={48}
         />
         <div className="nav-brand-name">
-          <div className="nav-name">Yash Patel</div>
-          <div>🇮🇳 [ UTC +05:30 ]</div>
+          <div className="nav-name">🇮🇳 Yash Patel</div>
+          <div> [ UTC +05:30 ]</div>
         </div>
       </div>
       <div className="nav-theme-switcher">
