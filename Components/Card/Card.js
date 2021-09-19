fabe8d33014f5c1698994eda174610c584f3c70e
ryuk156 <yp15601560@gmail.com>
2021-09-19 17:15:40 +0530

    fix: cards

--- Components/Card/Card.js
+++ Components/Card/Card.js
@@ -1,7 +1,14 @@
 
+import React,{useState} from "react"
+import Modal from "../Modal/Modal"
+
 const Card=()=>{
+
+  const [showModal,setShowModal] = useState(false)
+   
+
     return(
-        <div className="card">
+        <div className="card" onClick={()=>{setShowModal(!showModal)}}>
           <div className="card-content">
             <div className="card-item-a">
                 <img src="/images/me.png" className="card-img" />
@@ -11,6 +18,14 @@ const Card=()=>{
             </div>
           </div>
        
+          {
+            showModal===true?(
+             <Modal />
+            ):(
+              ""
+            )
+          }
+          
 
         </div>
     )
---
+++ Components/Modal/Modal.js
@@ -0,0 +1,14 @@
+
+
+const Modal=()=>{
+    return(
+        <div className="modal">
+        <div className="modal-content">
+           I'm modal
+        </div>
+     </div>
+    )
+
+}
+
+export default Modal
