8e6b8fdfdeefd81ecef8f6a781b52482413f905d
ryuk156 <yp15601560@gmail.com>
2021-09-28 00:19:53 +0530

    feat: added intro

--- Components/Container/Container.js
+++ Components/Container/Container.js
@@ -1,8 +1,8 @@
 /* eslint-disable react/no-unescaped-entities */
 import Board from "../Board/Board";
-import Link from "next/link"
+import Link from "next/link";
 
-const Container = ({content}) => {
+const Container = ({ content }) => {
   return (
     <div className="content_container">
       <div>
@@ -10,16 +10,24 @@ const Container = ({content}) => {
           Hey!<span className="waving-hand">👋</span>
         </div>
         <div className="intro_content">
-          I'm Yash and I'm UI Engineer and an open source contributor
+        I'm Yash and I'm UI Engineer and an open source contributor.
         </div>
-        <Board cardsData={content}/>
+        <p className="intro-details">
+        
+          I like to build things with other fun and creative people. I enjoy
+          solving complex problems through minimal solutions that have
+          deep impacts on individuals and communities.
+        </p>
+        
+        <Board cardsData={content} />
         <div className="experince-conatiner">
           <Link href={`/experience`}>
-
-        <div className="more-button">More <i className="fa fa-arrow-circle-right" aria-hidden="true"></i></div>
-        </Link>
+            <div className="more-button">
+              More{" "}
+              <i className="fa fa-arrow-circle-right" aria-hidden="true"></i>
+            </div>
+          </Link>
         </div>
-        
       </div>
     </div>
   );
