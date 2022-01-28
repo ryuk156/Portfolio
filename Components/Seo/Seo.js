9480e46350ba642ec10b2b1b4a36ee386eb7e1bb
ryuk156 <yp15601560@gmail.com>
2022-01-28 12:18:37 +0530

    feat: testing OG protocols

--- Components/Seo/Seo.js
+++ Components/Seo/Seo.js
@@ -1,18 +1,16 @@
 import Head from "next/head";
 
 const Seo = () => {
-  const siteName = "https://yash1560.me"
-  const pageTitle = "Hello I a developer"
   return (
     <Head>
       <link rel="icon" href="/favicon.ico" />
       <title>Yash Patel | UI Engineer</title>
-      <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
-     
-     <meta property="og:site_name" content={siteName} key="ogsitename" />
-      <meta property="og:title" content={pageTitle} key="ogtitle" />
+      <link
+        href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+        rel="stylesheet"
+      />
     </Head>
   );
 };
 
-export default Seo
+export default Seo;
